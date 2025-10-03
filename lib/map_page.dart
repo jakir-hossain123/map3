a1a6d5cf7c0fce0056e998bfee73261edacb0e99
Md.Jakir Hossain <jakir7z7z7z7z@gmail.com>
2025-10-03 10:34:30 +0600

    location

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -14,20 +14,64 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
  final Completer<GoogleMapController> _completer = Completer();
   static const  CameraPosition _myLocation = CameraPosition(
-    target: LatLng(51.5072, -0.1275),
-    zoom: 14// <--- Use the standard hyphen-minus (Unicode U+002D)zoom: 14
+    target: LatLng(23.76 , 90.38 ),
+    zoom: 14
   );
+  final List<Marker> myMarker = [];
+  final List<Marker> markerList = [
+    Marker(markerId: MarkerId('First'),
+    position: LatLng(23.76 , 90.38 ),
+      infoWindow: InfoWindow(title: 'I live in'),
+    ),
+    Marker(markerId: MarkerId('Second'),
+      position: LatLng(23.86, 90.00),
+      infoWindow: InfoWindow(title: 'MyHomeTown'),
+    ),
+    Marker(markerId: MarkerId('Third'),
+      position: LatLng(23.8583 , 90.2667 ),
+      infoWindow: InfoWindow(title: 'Savar'),
+    ),
+
+    //23.838047750820063, 89.78385693739705
+    Marker(markerId: MarkerId('Fourth'),
+      position: LatLng(23.8380, 89.7838 ),
+      infoWindow: InfoWindow(title: 'Sibaloy'),
+    ),
+
+  ];
+  void initState () {
+    super.initState();
+    myMarker.addAll(markerList);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body:
          GoogleMap(
             initialCameraPosition: _myLocation,
+           mapType: MapType.normal,
+           markers: Set<Marker>.of(myMarker),
            zoomControlsEnabled: false,
            onMapCreated: (GoogleMapController controller){
               _completer.complete(controller);
            },
         ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.location_searching),
+        onPressed: ()async{
+          GoogleMapController controller = await _completer.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+            const CameraPosition(
+              target: LatLng(23.86, 90.00),
+              zoom: 12
+          ),
+          ),
+          );
+          setState(() {
+
+          });
+      },
+      ),
       );
   }
 }
